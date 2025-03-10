edf418e55c9ddf4c3efbbbdc907bd050e1922479
Soumay-sanpui <somusanpui9@gmail.com>
2025-03-10 05:18:33 +0000

    updated 404

--- app/404/page.js
+++ app/404/page.js
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from 'next/link';
+
 const NotFoundPage = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-50">
@@ -7,12 +9,12 @@ const NotFoundPage = () => {
                 <h1 className="text-6xl font-extrabold text-gray-900">404</h1>
                 <p className="mt-4 text-lg text-gray-600">Oops! The page you are looking for does not exist.</p>
                 <p className="mt-2 text-gray-500">It might have been removed, or the URL might be incorrect.</p>
-                <a href="/" className="mt-6 inline-block bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200">
+                <Link href="/" className="mt-6 inline-block bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200">
                     Go Back Home
-                </a>
+                </Link>
             </div>
         </div>
     );
 };
 
-export default NotFoundPage; 
+export default NotFoundPage;
