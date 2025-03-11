fd600a27625d5783a05d3805d97d21e7b09fcbdc
Soumay-sanpui <somusanpui9@gmail.com>
2025-03-11 09:15:57 +0000

    added gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ComingSoon from '../(components)/ComingSoon'
+import Navbar from '../(components)/Navbar'
 
 const page = () => {
-  return (
-    <ComingSoon />
-  )
+    return (
+        // <div className="w-full py-6 px-8 relative bg-black text-white">
+        //     <Navbar />
+        //     <div className="w-full h-[5vw]"></div>
+        //     <h1 className="text-center text-2xl md:text-3xl lg:text-4xl my-4 md:my-6 lg:my-8 uppercase animate-fade-in font-monot">Team Gallery</h1>
+        //     <div className="columns-1 sm:columns-2 lg:columns-4 gap-3 mx-auto space-y-3 overflow-hidden">
+        //         {[
+        //             "/team_folder/1.webp",
+        //             "/team_folder/2.webp",
+        //             "/team_folder/3.webp",
+        //             "/team_folder/4.webp",
+        //             "/team_folder/5.webp",
+        //             "/team_folder/6.webp",
+        //             "/team_folder/7.webp",
+        //             "/team_folder/8.webp",
+        //             "/team_folder/9.webp",
+        //             "/team_folder/10.webp",
+        //             "/team_folder/11.webp",
+        //             "/team_folder/12.webp",
+        //             "/team_folder/13.webp",
+        //             "/team_folder/14.webp",
+        //             "/team_folder/15.webp",
+        //             "/team_folder/16.webp",
+        //             "/team_folder/17.webp",
+        //             "/team_folder/18.webp",
+        //             "/team_folder/19.webp",
+        //             "/team_folder/20.webp",
+        //             "/team_folder/21.webp",
+        //         ].map((src, index) => (
+        //             <div key={index} className="hover:scale-95 transition-all duration-500 cursor-pointer break-inside-avoid animate-slide-in" data-index={index}>
+        //                 <img loading={"lazy"} alt={`portfolio-image-${index}`} src={src} />
+        //             </div>
+        //         ))}
+        //     </div>
+        // </div>
+        <ComingSoon />
+    )
 }
 
 export default page
