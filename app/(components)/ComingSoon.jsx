9b102f052e7ea556061b38e32380067ebee77fa6
Soumay-sanpui <somusanpui9@gmail.com>
2025-03-11 07:35:44 +0000

    added coming soon

---
+++ app/(components)/ComingSoon.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ComingSoon = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden select-none">
+        <Navbar />
+        <div className="w-full h-full flex items-center justify-center overflow-hidden bg-gradient-to-t from-red-600 to-black">
+            <h1 className="text-8xl font-monot text-white uppercase">coming soon</h1>
+        </div>
+    </div>
+  )
+}
+
+export default ComingSoon
